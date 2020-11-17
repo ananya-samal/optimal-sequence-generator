26fc054c0f26d245593b98a66bc92bd9728cde57
ananya-samal <34180878+ananya-samal@users.noreply.github.com>
2020-11-17 14:39:32 +0100

    Update Task-1-C-optimize-seriesgeneration.py
    Added a new approach which consumes a little less time than previous

--- Task-1-C-optimize-seriesgeneration.py
+++ Task-1-C-optimize-seriesgeneration.py
@@ -10,10 +10,11 @@ Description:
   2- Find the fastest time required to reach 1.
   
 Remark:
- 1- This is optimised version of task b.
+ Adding more than one approach to understand the time efficiency on each approach
 '''
 import timeit                             #using timeit module for time benchmarking
 
+#First solution using division 
 def optimal_sequence_desc_with_div(n):    
     evaluate = True
     step_cnt = 0                          #initialising the setup count to 0 
@@ -35,4 +36,33 @@ for n in range(1,10000,1):                     #for loop to iterate in number 1 
 end_total_time = timeit.default_timer();        #end time after execution
 total_execution_time = (end_total_time - start_total_time) #time needed for entire calculation
 print("optimal_sequence_desc_with_div::The execution time to reach 1 in all sequences is "+str(total_execution_time)) 
-print(optimal_steps_array)
+#print(optimal_steps_array)  #commented out for readbility uncomment to see the list of step count required for each no
+
+##second approach little time improvement is acheieved when loop is moved inside the function
+def optimal_sequence_desc_with_div_looped():
+    
+    def optimal_sequence_desc_with_div_int(n): #to implemet map iteration defined this function
+        evaluate = True
+        step_cnt = 0
+        new_elem_sequence = n
+        while(evaluate == True):
+            new_elem_sequence = (new_elem_sequence * 3) + 1 if new_elem_sequence % 2 != 0 else new_elem_sequence // 2
+            step_cnt = step_cnt + 1
+            if(new_elem_sequence == 1):
+                evaluate = False
+        return step_cnt                
+    
+    optimal_steps_array1 = []
+    optimal_steps_array1 = list(map(optimal_sequence_desc_with_div_int, list(range(1,10000,1)) )) #converting the list to map for easy retrieval
+    print(type(optimal_steps_array1)) #type is list
+    #print(optimal_steps_array1)    #commented out for readbility uncomment to see the list of step count required for each no
+    return optimal_steps_array1     #return type is a list
+
+#Q(c)
+##Optimization 2 - Move for loop inside function
+start_total_time = timeit.default_timer();
+optimal_steps_array_looped = optimal_sequence_desc_with_div_looped()
+print(type(optimal_steps_array_looped))
+end_total_time = timeit.default_timer();
+total_execution_time = (end_total_time - start_total_time)
+print("optimal_sequence_desc_with_div_looped::The execution time to reach 1 in all sequences is "+str(total_execution_time))
